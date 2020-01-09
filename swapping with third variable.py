e38d68da4ed5744a47c5835731ee798eaa8558f0
ambersanghvi <55327772+ambersanghvi@users.noreply.github.com>
2020-01-09 16:33:32 +0530

    Add files via upload

---
+++ swapping with third variable.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+t = a
+a = b
+b = t
+print("Numbers after swapping : a=" , a, "b=", b)
---
+++ swapping without third variable.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+a , b = b,a
+print("Numbers after swapping : a=" , a, "b=", b)
